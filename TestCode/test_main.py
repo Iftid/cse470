b2fec2da13f8a31357d88fb295f295932f43e932
Iftid <103838055+Iftid@users.noreply.github.com>
2022-04-30 17:54:20 +0600

    Update test_main.py

--- TestCode/test_main.py
+++ TestCode/test_main.py
@@ -1,21 +1,72 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
-        self.assertEqual(True, True)
-        print("this method works fine")
 
-    def test_something2(self):
-       self.assertEqual(True, True)
-       self.assertEqual(True, True)
-       print("this method works fine")
+class Test_main(unittest.TestCase):
 
+    
+
+    def setUp(self):
+        print('setUp')
+        self.emp_1 = Employee('Corey', 'Schafer', 50000)
+        self.emp_2 = Employee('Sue', 'Smith', 60000)
+
+    def tearDown(self):
+        print('tearDown\n')
+
+    def test_email(self):
+        print('test_email')
+        self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
+        self.assertEqual(self.emp_2.email, 'Sue.Smith@email.com')
+
+        self.emp_1.first = 'John'
+        self.emp_2.first = 'Jane'
+
+        self.assertEqual(self.emp_1.email, 'John.Schafer@email.com')
+        self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
+
+    def test_fullname(self):
+        print('test_fullname')
+        self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
+        self.assertEqual(self.emp_2.fullname, 'Sue Smith')
+
+        self.emp_1.first = 'John'
+        self.emp_2.first = 'Jane'
+
+        self.assertEqual(self.emp_1.fullname, 'John Schafer')
+        self.assertEqual(self.emp_2.fullname, 'Jane Smith')
+
+    def test_apply_raise(self):
+        print('test_apply_raise')
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
+
+        self.assertEqual(self.emp_1.pay, 52500)
+        self.assertEqual(self.emp_2.pay, 63000)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+###### setUpClass and tearDownClass ######
+
+@classmethod
+def setUpClass(cls):
+    print('setupClass')
+
+@classmethod
+def tearDownClass(cls):
+    print('teardownClass')
+
+
+##### Mocking #####
+def monthly_schedule(self, month):
+    response = requests.get(f'http://company.com/{self.last}/{month}')
+    if response.ok:
+        return response.text
+    else:
+        return 'Bad Response!'
 
 
 
